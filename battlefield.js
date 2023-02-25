b19546c04923125e6a2daa4c145c467aa14afbbe
junkdeveloper <q906107506@gmail.com>
2023-02-25 17:24:47 -0600

    newupdate

--- battlefield.js
+++ battlefield.js
@@ -1,13 +1,31 @@
 var dice = document.querySelector(".dice")
 
+
 dice.addEventListener("click", () => {
     game()
+    updateRound()
 })
 
 function game() {
     let num = generateNum(1, 6)
     rollDice(num)
 }
+
+function gameStart() {
+    choosechr.parentElement.style.display = "none"
+    title.style.visibility = "visible"
+    updatePlayer(players[s].name)
+    writeInfo()
+    placeInfo()
+    person = players[0]
+}
+
+function updatePlayer(name) {
+    title.innerHTML = name
+    title.style.background = colorScheme[name]
+}
+
+
 
 function rollDice(num) {
     let bg = generateNum(1, 2)
@@ -27,4 +45,9 @@ function toggleDice(state) {
 
 function generateNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min
+}
+
+function updateRound() {
+    let num = +document.querySelector('.process span b').innerHTML + 1
+    document.querySelector('.process span b').innerHTML = num
 }
